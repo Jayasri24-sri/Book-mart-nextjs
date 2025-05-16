d25d773a8fa84f5723b7d433972bb6c61d69fe89
Jayasri <jayasri@recuitly.io>
2025-05-16 16:10:18 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import SubHeader from "@/components/SubHeader";
-
 const inter = Inter({ subsets: ['latin'] })
-
 export const metadata: Metadata = {
     title: 'BookMart',
     description: 'Buy and sell books online',
